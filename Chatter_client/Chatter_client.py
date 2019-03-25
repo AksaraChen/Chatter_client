e2e94f21a0918ec6eb26fd1c874877fba85e04a8
則叡 陳 <chenray603@gmail.com>
2019-03-25 08:18:07 +0800

    client version 0.5

--- Chatter_client/Chatter_client.py
+++ Chatter_client/Chatter_client.py
@@ -1,1 +1,29 @@
+import threading
+import socket
 
+HOST = "127.0.0.1"
+PORT = 50000
+global s
+
+def recieve(conn):
+    while True:
+        data=conn.recv(1024)
+        if not data:
+            break
+        print(data)
+
+
+
+def main():
+    global s
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+
+    T=threading.Thread(target=recieve,args=(s,))
+    T.start()
+    ID=input("請輸入ID：")
+    s.send(str.encode("ID_register"))
+    s.send(str.encode(ID))
+
+if __name__=="__main__":
+    main()
