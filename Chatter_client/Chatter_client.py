0bb9a2dbf476a30ec96c7b3e7bc78b35ff3f520f
Ray <chanray603@gmail.com>
2019-03-27 07:40:50 +0800

    增加查詢ip功能 加入連線失敗error message

--- Chatter_client/Chatter_client.py
+++ Chatter_client/Chatter_client.py
@@ -1,31 +1,39 @@
 import threading
 import socket
 
+
 HOST = "127.0.0.1"
 PORT = 50000
 global s
 
 def recieve(conn):
     while True:
-        data=conn.recv(1024)
-        if not data:
+        try:
+            data=conn.recv(1024)
+            if not data:
+                break
+            print(str(data,encoding="utf-8"))
+        except:
+            print("\n連線失敗")
             break
-        print(str(data,encoding="utf-8"))
 
 
 
 def main():
     global s
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((HOST,PORT))
+    try:
+        s.connect((HOST,PORT))
+        T=threading.Thread(target=recieve,args=(s,))
+        T.start()
+        ID=input("請輸入ID：")
+        s.send(str.encode("ID_register"))
+        s.send(str.encode(ID))
+        while True:
+            message=input("若想查詢自身IP，請輸入\myip，若想查詢對方ip，請輸入\herip\n請輸入訊息:\n")
+            s.send(str.encode(message))
+    except:
+        print("\n連線失敗")
 
-    T=threading.Thread(target=recieve,args=(s,))
-    T.start()
-    ID=input("請輸入ID：")
-    s.send(str.encode("ID_register"))
-    s.send(str.encode(ID))
-    while True:
-        message=input("請輸入訊息:\n")
-        s.send(str.encode(message))
 if __name__=="__main__":
     main()
